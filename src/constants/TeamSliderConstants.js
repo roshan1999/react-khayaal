88504c424b69c35a7a59be6d88cded9ff1b0cbea
roshan1999 <jacsroshan@gmail.com>
2022-06-19 13:38:38 +0530

    Members updated

--- src/constants/TeamSliderConstants.js
+++ src/constants/TeamSliderConstants.js
@@ -1,13 +1,23 @@
-import {memberImages, getLandingAboutImages} from '../images.js';
+import {memberImages} from '../images.js';
 
 export function getMembers() {
     let members = [];
-    let name = ["Roshan","Rohan","Pariakhar","Jacob","Manoj"];
-    let position = ["President - IT", "Founder", "Co-founder", "CTO", "Developer"]
+    let name = [
+        "Uday Somaiya", "Sharmishtha Singh", "Rohan Pariakhar",
+        "Pallak Bhutani","Viraj Gaikwad","Janvi Bhanushali",
+        "Sanika Kamble","Iram Dhanani","Mohammed Rangwala",
+        "Khushi Mundra","Divyanshi Chopra"];
+    let position = [
+        "President for Strategy", "Founder", "Co-Founder",
+        "Campaign President","President for Public Relations",
+        "President for Content Writing","President for Design Team",
+        "President for Content Creators","President of Human Resources",
+        "Vice President for Education Campaign","Vice President for Content Writing"
+    ]
     memberImages.map((image, index) => {
         members.push({
             image: image,
-            name: name[index], 
+            name: name[index],
             position: position[index]
         })
     })
--- src/images.js
+++ src/images.js
@@ -1,13 +1,24 @@
-import memberOne from './assets/Members/member1.jpg';
-import memberTwo from './assets/Members/member2.jpg';
-import memberThree from './assets/Members/member3.jpg';
-import memberFour from './assets/Members/member4.jpg';
-import memberFive from './assets/Members/member5.jpg';
+import rohan from './assets/Members/rohan.jpg';
+import sharmishtha from './assets/Members/sharmishtha.jpg';
+import pallak from './assets/Members/pallak.jpg';
+import uday from './assets/Members/uday.png';
+import viraj from './assets/Members/viraj.png';
+import mohammad from './assets/Members/mohammad.png';
+import khushi from './assets/Members/khushi.png';
+import janvi from './assets/Members/janvi.png';
+import iram from './assets/Members/iram.png';
+import divyanshi from './assets/Members/divyanshi.png';
+import sanika from './assets/Members/sanika.png';
 import khayaalOne from './assets/Homepage/AboutKhayaal/khayaal-about-1.png';
 import khayaalTwo from './assets/Homepage/AboutKhayaal/khayaal-about-2.png';
 import khayaalThree from './assets/Homepage/AboutKhayaal/khayaal-about-3.png';
 
-const memberImages =  [memberOne, memberTwo, memberThree, memberFour, memberFive];
+const memberImages = [
+    uday, sharmishtha, rohan,
+    pallak,viraj,janvi,
+    sanika,iram,mohammad,
+    khushi,divyanshi
+];
 const getLandingAboutImages = [khayaalOne, khayaalTwo, khayaalThree];
 
-export {memberImages, getLandingAboutImages};
+export { memberImages, getLandingAboutImages };
